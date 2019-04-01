166f4f2b4eeb6ce5a580bf52bc1c3177be5bd319
晨光熹微 <brave_cgx@163.com>
2019-04-01 22:17:57 +0800

    first commit

---
+++ chain_table/DList.js
@@ -0,0 +1,14 @@
+class DList {
+    constructor() {
+        this.head = null;
+        this.trail = null;
+    }
+}
+
+class Node {
+    constructor() {
+        this.value = null;
+        this.next = null;
+        this.prev = null;
+    }
+}
---
+++ chain_table/SList.js
@@ -0,0 +1,12 @@
+class SList {
+    constructor() {
+        this.head = null;
+    }
+}
+
+class Node {
+    constructor() {
+        this.value = null;
+        this.next = null;
+    }
+}
