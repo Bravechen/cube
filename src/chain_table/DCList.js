955cfdf6fff15e6b0692f599c2ccc04707874f30
Brave Chen <brave_cgx@163.com>
2019-04-16 11:02:53 +0800

    develop #DCList.insertAt() develop #DCList.removeAt() test DCList.js

--- src/chain_table/DCList.js
+++ src/chain_table/DCList.js
@@ -55,15 +55,83 @@ class DCList {
     return -1;
   }
 
-  insertAt(position = -1, value) {}
+  insertAt(position = -1, value) {
+    if (position < 0 || position >= this.length) {
+      return;
+    }
+    let node = new Node(value);
+    let mid = this.length>>1;
+    let current = getLinkedNode(
+      position,
+      position > mid ? this.trail : this.head,
+      this.length,
+      position > mid ? -1 : 1
+    );
+    let prev = current.prev;
+    prev.next = node;
+    node.prev = prev;
+    node.next = current;
+    current.prev = node;
+    if (position === 0) {
+      this.head = node;
+    }
 
-  removeAt(position = -1, value) {}
+    if (position === this.length -1) {
+      this.trail = node;
+    }
+    this.length++;
+  }
 
-  remove(value) {}
+  removeAt(position = -1) {
+    if (position < 0 || position >= this.length) {
+      return;
+    }
+    let mid = this.length >> 1;
+    let current = getLinkedNode(
+      position,
+      position > mid ? this.trail : this.head,
+      this.length,
+      position > mid ? -1 : 1
+    );
+    let prev = current.prev;
+    let next = current.next;
+    prev.next = next;
+    next.prev = prev;
+    if (position === 0) {
+      this.head = next;
+    }
 
-  traversal() {}
+    if (position === this.length - 1) {
+      this.trail = prev;
+    }
+    this.length--;
+    return current.value;
+  }
 
-  destroy() {}
+  remove(value) {
+    let index = this.indexOf(value);
+    if (index < 0) {
+      return;
+    }
+    return this.removeAt(index);
+  }
+
+  traversal() {
+    let node = this.head;
+    let index = 0;
+    let ary = [];
+    while (index++ < this.length) {
+      ary.push(node.value);
+      node = node.next;
+    }
+    return ary;
+  }
+
+  destroy() {
+    this.head = null;
+    this.trail = null;
+    this.length = 0;
+  }
 }
 
 class Node {
@@ -72,6 +140,18 @@ class Node {
     this.prev = null;
     this.next = null;
   }
+}
+
+function getLinkedNode(position, start, length, direct) {
+  let current = start;
+  let index = direct === -1 ? length - 1 : 0;
+  let key = direct === -1 ? 'prev' : 'next';
+  let step = direct === -1 ? -1 : 1;
+  while (index !== position) {
+    current = current[key];
+    index += step;
+  }
+  return current;
 }
 
 module.exports = DCList;
--- test/dclist.js
+++ test/dclist.js
@@ -82,4 +82,89 @@ describe('DCList', function() {
       expect(dclist.lastIndexOf(1)).to.equal(0);
     });
   });
+
+  describe('#DCList.insertAt(position, value)', function() {
+
+    it('after insert, property length equal 11', function() {
+      let dclist = createDCList();
+      dclist.insertAt(6, 12);
+      expect(dclist.length).to.equal(11);
+    });
+
+    it('after insert 12 at index 6, will get 12 at index 6', function() {
+      let dclist = createDCList();
+      dclist.insertAt(6, 12);
+      expect(dclist.indexOf(12)).to.equal(6);
+    });
+
+    it('after insert 11 at index 0, dclist.head.value equal 11', function() {
+      let dclist = createDCList();
+      dclist.insertAt(0, 11);
+      expect(dclist.head)
+        .to.be.a('object')
+        .have.property('value')
+          .equal(11);
+    });
+
+    it('after insert 12 at dclist.length-1, dclist.trail.value equal 12', function() {
+      let dclist = createDCList();
+      dclist.insertAt(dclist.length-1, 12);
+      expect(dclist.trail)
+        .to.be.a('object')
+        .have.property('value')
+          .equal(12);
+    });
+
+  });
+
+  describe('#DCList.removeAt(position, value)', function() {
+    it('after invoke removeAt(), property length equal 9', function() {
+      let dclist = createDCList();
+      dclist.removeAt(5);
+      expect(dclist.length).to.equal(9);
+    });
+
+    it('after invoke removeAt(0), property length not change that equal 10', function() {
+      let dclist = createDCList();
+      dclist.removeAt(-1);
+      expect(dclist.length).to.equal(10);
+    });
+
+    it('after invoke removeAt(dclist.length), property length not change that equal 10', function() {
+      let dclist = createDCList();
+      dclist.removeAt(dclist.length);
+      expect(dclist.length).to.equal(10);
+    });
+
+    it('remove value 5 at index 4, back value equal 5', function() {
+      let dclist = createDCList();
+      let value = dclist.removeAt(4);
+      expect(value).to.equal(5);
+    });
+
+    it('remove value 5 at index 4, get value 6 at index 4', function() {
+      let dclist = createDCList();
+      let value = dclist.removeAt(4);
+      console.log(dclist.traversal());
+      expect(dclist.indexOf(6)).to.equal(4);
+    });
+
+    it('remove value 1 at index 0, dclist.head.value equal 2', function() {
+      let dclist = createDCList();
+      let value = dclist.removeAt(0);
+      expect(dclist.head)
+        .to.be.a('object')
+        .have.property('value')
+          .equal(2);
+    });
+
+    it('remove value 10 at index dclist.length - 1, dclist.trail.value equal 9', function() {
+      let dclist = createDCList();
+      let value = dclist.removeAt(dclist.length - 1);
+      expect(dclist.trail)
+        .to.be.a('object')
+        .have.property('value')
+          .equal(9);
+    });
+  });
 });
